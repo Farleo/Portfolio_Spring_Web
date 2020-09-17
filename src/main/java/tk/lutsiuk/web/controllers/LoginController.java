1fb039eaec1c257808d8d94106d1364f03b4aeb1
Taras <farleo89@gmail.com>
2020-09-17 15:58:16 +0300

    add auto login after registration new user

--- src/main/java/tk/lutsiuk/web/controllers/LoginController.java
+++ src/main/java/tk/lutsiuk/web/controllers/LoginController.java
@@ -1,22 +1,36 @@
 package tk.lutsiuk.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import tk.lutsiuk.web.models.Role;
 import tk.lutsiuk.web.models.User;
 import tk.lutsiuk.web.repository.UserRepository;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Map;
+
+import static tk.lutsiuk.web.models.Role.USER;
 
 @Controller
 public class LoginController {
 
 @Autowired
+@Qualifier("authenticationManager")
+protected AuthenticationManager authenticationManager;
+@Autowired
 private UserRepository userRepository;
+@Autowired
+private PasswordEncoder passwordEncoder;
 
 @GetMapping("/login")
 public String getLoginHands(@AuthenticationPrincipal User currentUser) {
@@ -32,15 +46,23 @@ public String getRegistration() {
 }
 
 @PostMapping("/registration")
-public String addRegistrationUser(User user, Map<String, Object> model) {
+public String addRegistrationUser(User user, Map<String, Object> model, HttpServletRequest request) {
 	User userFromDb = userRepository.findByEmail(user.getEmail());
 	if (userFromDb != null) {
 		model.put("message", "User exist!");
 		return "registration";
 	}
 	user.setActive(true);
-	user.setRoles(Collections.singleton(Role.USER));
+	user.setRoles(Collections.singleton(USER));
+	user.setPassword(passwordEncoder.encode(user.getPassword()));
 	userRepository.save(user);
-	return "redirect:/login";
+	authenticateUserAndSetSession(user);
+	return "redirect:/";
+}
+
+private void authenticateUserAndSetSession(User user) {
+	Authentication authentication = new UsernamePasswordAuthenticationToken(user, null,
+			AuthorityUtils.createAuthorityList("USER"));
+	SecurityContextHolder.getContext().setAuthentication(authentication);
 }
 }
