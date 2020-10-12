76d5da3d412168dc952f00683d81e8b5392dd94f
Taras <farleo89@gmail.com>
2020-10-12 23:18:13 +0300

    add main page. change style on menu nav.

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -36,3 +36,16 @@ function hasScrolled() {
 
     lastScrollTop = st;
 }
+//change color on active link menu in header
+$(function () {
+    var location = window.location.href;
+    var cur_url = '/' + location.split('/').pop();
+
+    $('.nav li').each(function () {
+        var link = $(this).find('a').attr('href');
+
+        if (cur_url === link) {
+            $(this).addClass('active');
+        }
+    });
+});
